b1ce56e6989d918f074a480f72c6c94131f502d0
Martin Budinsky <martin.budinsky@memsource.com>
2022-02-15 14:05:01 +0100

    add naive implementation without validation

--- screens/NaiveScreen.tsx
+++ screens/NaiveScreen.tsx
@@ -1,23 +1,28 @@
-import { Switch, TextInput, TouchableOpacity } from 'react-native';
+import { Switch, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { commonFormStyles as styles } from '../styles/commonForm';
 import { RootTabScreenProps } from '../types';
+import {useState} from "react";
 
 export default function NaiveScreen({ navigation }: RootTabScreenProps<'Naive'>) {
+  const [emailInputText, setEmailInputText] = useState<string>('me@example.com');
+  const [messageInputText, setMessageInputText] = useState<string>('ahojky');
+  const [doesUserAgree, setDoesUserAgree] = useState<boolean>(false);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Naive implementation</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
       <Text>Email</Text>
-      <TextInput placeholder="me@example.com" style={styles.input}/>
+      <TextInput value={emailInputText} onChangeText={setEmailInputText} placeholder="me@example.com" style={styles.input}/>
       <Text>Message</Text>
-      <TextInput placeholder="Hello There!" style={styles.input}/>
+      <TextInput value={messageInputText} onChangeText={setMessageInputText} placeholder="Hello There!" style={styles.input}/>
       <Text>Agreement</Text>
-      <Switch/>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Modal', {
-        email: 'some@email.com',
-        message: 'General Kenobi!',
-      })}>
+      <Switch value={doesUserAgree} onValueChange={setDoesUserAgree}/>
+      <TouchableOpacity style={styles.button} onPress={() => doesUserAgree ? navigation.navigate('Modal', {
+        email: emailInputText,
+        message: messageInputText,
+      }) : Alert.alert('You have to agree with us!')}>
         <Text style={styles.buttonText}>Send it!</Text>
       </TouchableOpacity>
     </View>
