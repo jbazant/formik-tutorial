648ef94ebd7eb9fd5da645bdde95b1c88ac57b6b
Martin Budinsky <martin.budinsky@memsource.com>
2022-02-15 15:09:43 +0100

    add Formik implementation

--- screens/FormikScreen.tsx
+++ screens/FormikScreen.tsx
@@ -2,25 +2,38 @@ import { Switch, TextInput, TouchableOpacity } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { commonFormStyles as styles } from '../styles/commonForm';
 import { RootTabScreenProps } from '../types';
+import {useForm} from "../hooks/useForm";
+import {Formik} from "formik";
 
 export default function FormikScreen({ navigation }: RootTabScreenProps<'Formik'>) {
+
+  const onPress = ({email, message}: {email: string, message: string, doesUserAgree: boolean}) => {
+    navigation.navigate('Modal', {
+      email, message
+    })};
+
   return (
+    <Formik
+      initialValues={{email:"me@example.com", message: "ahojky", doesUserAgree: false }}
+      onSubmit={onPress}
+    >
+      {({ handleChange, setFieldValue, handleBlur, handleSubmit, values }) =>
+        (
     <View style={styles.container}>
-      <Text style={styles.title}>Formik usage</Text>
+      <Text style={styles.title}>Better implementation</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
       <Text>Email</Text>
-      <TextInput placeholder="me@example.com" style={styles.input}/>
+      <TextInput value={values.email} onChangeText={handleChange('email')} placeholder="me@example.com" style={styles.input}/>
       <Text>Message</Text>
-      <TextInput placeholder="Hello There!" style={styles.input}/>
+      <TextInput value={values.message} onChangeText={handleChange('message')} placeholder="Hello There!" style={styles.input}/>
       <Text>Agreement</Text>
-      <Switch/>
-      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Modal', {
-        email: 'some@email.com',
-        message: 'General Kenobi!',
-      })}>
+      <Switch value={values.doesUserAgree} onValueChange={ value => setFieldValue('doesUserAgree', value)}/>
+      <TouchableOpacity style={styles.button} onPress={() => handleSubmit()}>
         <Text style={styles.buttonText}>Send it!</Text>
       </TouchableOpacity>
     </View>
+        )}
+  </Formik>
   );
 }
 
